0b8ea609659391d361b08660459940fd40951240
DanielFordHUB <105242930+DanielFordHUB@users.noreply.github.com>
2022-07-27 14:31:11 -0500

    fixed missing function

--- explore.py
+++ explore.py
@@ -1,6 +1,7 @@
 import itertools
 import seaborn as sns
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 
@@ -73,3 +74,33 @@ def joint(x, y, df):
     '''
     sns.jointplot(x = x, y = y, data = df, kind = 'reg',
                   joint_kws = {'line_kws':{'color':'orange'}});
+    
+def value_correlations(train):
+    '''
+    This functino takes in the zillow train sample and uses pandas and seaborn to create a
+    ordered list and heatmap of the correlations between the various quantitative feeatures and the target. 
+    '''
+    # create a dataframe of correlation values, sorted in descending order
+    corr = pd.DataFrame(train.corr().abs().tax_value).sort_values(by='tax_value', ascending=False)
+    # rename the correlation column
+    corr.columns = ['correlation (abs)']
+    # establish figure size
+    plt.figure(figsize=(10,10))
+    # creat the heatmap using the correlation dataframe created above
+    sns.heatmap(corr, annot=True, cmap="mako")
+    # establish a plot title
+    plt.title('Features\' Correlation with Value')
+    # display the plot
+    plt.show()
+    
+    
+def significance_test(p):
+    '''
+    Assumes an alpha of .05. Takes in a value, p, and returns a string stating whether or not that p value indicates sufficient evidence
+    to reject a null hypothesis.
+    '''
+    α = 0.05
+    if p < α:
+        print("Sufficient evidence -> Reject the null hypothesis.")
+    else:
+        print("Insufficient evidence -> Fail to reject the null hypothesis.")
